b80a0dc54ead7cc5ea3186fa63dffb19dca268b3
Kelly Vergara <kellyvergara12@gmail.com>
2023-06-04 23:00:35 -0400

    crudAdd project files.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -66,10 +68,10 @@ namespace FarmersMarket
             try
             {
                 con.Open();
-                string Query = "insert into products values(@id, @productName, @amount, @price)";
+                string Query = "insert into products values(@id, @product_name, @amount, @price)";
                 cmd = new NpgsqlCommand(Query, con);
                 cmd.Parameters.AddWithValue("@id", int.Parse(InsertID.Text));
-                cmd.Parameters.AddWithValue("@productName", InsertPName.Text);
+                cmd.Parameters.AddWithValue("@product_name", InsertPName.Text);
                 cmd.Parameters.AddWithValue("@amount",int.Parse(InsertAmount.Text));
                 cmd.Parameters.AddWithValue("@price",double.Parse(InsertPrice.Text));
                 cmd.ExecuteNonQuery();
@@ -82,17 +84,122 @@ namespace FarmersMarket
 
         private void DeleteSQL_Click(object sender, RoutedEventArgs e)
         {
+            establishConnection();
+            try
+            {
+                con.Open();
+                string Query = "Delete from products where id=@id";
+                cmd = new NpgsqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@id",int.Parse(InsertID.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Succesfully deleted from the Farmer's Market databse");
+                con.Close();
 
+            }catch(NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void UpdateSQL_Click(object sender, RoutedEventArgs e)
         {
-
+            establishConnection();
+            try
+            {
+                con.Open();
+                string Query = "UPDATE public.products SET product_name=@product_name, amount=@amount, price=@price WHERE id=@id"; 
+                cmd = new NpgsqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@product_name", InsertPName.Text);
+                cmd.Parameters.AddWithValue("@amount", int.Parse(InsertAmount.Text));
+                cmd.Parameters.AddWithValue("@price", double.Parse(InsertPrice.Text));
+                cmd.Parameters.AddWithValue("@id", int.Parse(InsertID.Text));
+                MessageBox.Show("Succesfully updated in the Farmer's Market databse");
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void SelectSQL_Click(object sender, RoutedEventArgs e)
         {
+            // Establish the Connection
+            establishConnection();
+            // Open the Connection
+            try
+            {
+                con.Open();
+                string Query = "select * from products";
+                cmd = new NpgsqlCommand(Query, con);
+                /*
+                 * as we are going to view our data table entries in GridView, we need
+                 * a dataapater to pull the data entries and view them in the GridView
+                 In the data adapter, you need to pass the command adpater you have created
+                for your program. here, cmd is our commandadapter
+                 
+                After having the dataadpater, we need to create a datatable instance and
+                add all our pulled record to that table, We need these table to 
+                information our datagridview to know what columns and rows its going to view
+                as table
+                 */
+                NpgsqlDataAdapter npgsqlDataAdapter = new NpgsqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                npgsqlDataAdapter.Fill(dt); // this line helps to retrive the table
+                // we have created using the Data adapter, pass the structure to our
+                // regular datatable and get all the values to that table
 
+                // The following line will help us to add the values to the dataGrid view
+                dataGrid.ItemsSource = dt.AsDataView();
+
+                //This will perform the dynamic binding, It will update the grid with the
+                // passed data table information
+                DataContext = npgsqlDataAdapter;
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+
+        private void sales_Click(object sender, RoutedEventArgs e)
+        {   
+            Sale newSale = new Sale();
+            newSale.Show();
+            this.Close();   
+        }
+
+        
+        private async void  SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            establishConnection();
+            try
+            {
+                con.Open();
+                string Query = "select * from products where id=@id";
+                // command adapter
+                cmd = new NpgsqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(searchProductID.Text));
+
+                
+                 /* in this program, we are going to add a reader to read one entry from the 
+                 database*/
+                 
+                var reader = await cmd.ExecuteReaderAsync();
+
+                InsertID.Text = reader.GetOrdinal("id").ToString();
+                InsertPName.Text = reader.GetOrdinal("product_name").ToString();
+                InsertAmount.Text = reader.GetOrdinal("amount").ToString();
+                InsertPrice.Text = reader.GetOrdinal("price").ToString();
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
     }
 }
---
+++ Sale.xaml.cs
@@ -0,0 +1,160 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarmersMarket
+{
+    /// <summary>
+    /// Interaction logic for Sale.xaml
+    /// </summary>
+    public partial class Sale : Window
+    {
+
+        public static NpgsqlConnection con;
+        public static NpgsqlCommand cmd;
+        private DataGrid salesDataGrid { set; get; }
+        private DataTable productsTable;
+    
+
+        public Sale()
+        {
+
+            // Initialize database connection
+            string connectionString = "Host=localhost;Port=5432;Database=farmersMarket;Username=postgres;Password=0000;";
+            con = new NpgsqlConnection(connectionString);
+
+            productsTable = new DataTable();
+
+            InitializeComponent();
+           
+            LoadProductsData();
+
+        }
+
+        private static string getConnectionString()
+        {
+            string connectString = "Host=localhost;Port=5432;Database=farmersMarket;Username=postgres;Password=0000;";
+            return connectString;
+        }
+
+
+        private static void establishConnection()
+        {
+            try
+            {
+                con = new NpgsqlConnection(getConnectionString());
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+
+        public void LoadProductsData()
+        {
+            try
+            {
+                con.Open();
+                string query = "SELECT id AS Product_ID, product_name AS Product_Name, price AS Price, amount AS Amount_Desired FROM products";
+                cmd = new NpgsqlCommand(query, con);
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(cmd);
+                adapter.Fill(productsTable);           
+
+                foreach (DataRow row in productsTable.Rows)
+                {
+                    row["Amount_Desired"] = 0;
+                }
+                salesDataGrid = dataGridSale;
+                salesDataGrid.ItemsSource = productsTable.DefaultView;
+
+                DataContext = adapter;
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void CalculateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double total = 0;
+
+            foreach (DataRow row in productsTable.Rows)
+            {
+                double amount = Convert.ToDouble(row["Amount_Desired"]);
+                double price = Convert.ToDouble(row["Price"]);
+                total += amount * price;
+            }
+
+            showTotal.Content = "$ " + total.ToString("0.00");
+        }
+
+
+        private void BuyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string customerName = search.Text;
+            string message = $"Thank you, {customerName}. Your purchase was successfully processed.";
+
+            try
+            {
+                con.Open();
+
+                foreach (DataRow row in productsTable.Rows)
+                {
+                    int productId = Convert.ToInt32(row["Product_ID"]);
+                    int amountDesired = Convert.ToInt32(row["Amount_Desired"]);
+
+                    // Update the database with the deducted amount
+                    string updateQuery = "UPDATE products SET amount = amount - @amountDesired WHERE id = @productId";
+                    cmd = new NpgsqlCommand(updateQuery, con);
+                    cmd.Parameters.AddWithValue("@amountDesired", amountDesired);
+                    cmd.Parameters.AddWithValue("@productId", productId);
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show(message);
+
+                // Clear the amount column in the data grid
+                foreach (DataRow row in productsTable.Rows)
+                {
+                    row["Amount_Desired"] = 0;
+                }
+
+                // Refresh the data grid
+                salesDataGrid.Items.Refresh();
+                showTotal.Content = "$ 0.0 ";
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void _return_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            Application.Current.MainWindow = mainWindow;
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
